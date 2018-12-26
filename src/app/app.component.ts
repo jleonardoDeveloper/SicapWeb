8b10d698fbd2bf95b4eab1c1dfa1e24b3379f441
jleonaroDeveloper <1012280@utp.edu.pe>
2018-12-26 16:47:37 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+export interface MenuItem {
+  name: string;
+  icon: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'SicapWeb';
+  menuItems:MenuItem[] =[
+    {
+      name:"Inicio",
+      icon: "home",
+      url:"/"
+    },
+    {
+      name:"Asistencias",
+      icon: "assignment_ind",
+      url:"/"
+    },
+    {
+      name:"Soporte",
+      icon: "playlist_add_check",
+      url:"/"
+    },
+    {
+      name:"Reportes",
+      icon: "bar_chart",
+      url:"/"
+    },
+  ];
+  ngOnInit() {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
